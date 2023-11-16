527e5b584f46ed12487d1f56a0768dde04a2007c
niko0816 <nt22304016@ga.ttc.ac.jp>
2023-11-16 16:07:52 +0900

    quiz系以外のbeanの作成しました

--- quizoo/src/main/java/db/bean/UserInfoBean.java
+++ quizoo/src/main/java/db/bean/UserInfoBean.java
@@ -1,5 +1,71 @@
-package db.bean; 
-public class UserInfoBean { 
+package db.bean;
+
+import java.io.Serializable;
+
+public class UserInfoBean implements Serializable{ 
     private String userId; 
     private int userNo;  
+    private String nickname;
+    private String password;
+    private int totalAnswer;
+    private int correctAnswer;
+    private float rating;
+    
+    public UserInfoBean() {}
+    
+    public UserInfoBean(String userId, int userNo, String nickname, String password, int totalAnswer, int correctAnswer, float rating) {
+    	this.userId = userId;
+    	this.userNo = userNo;
+    	this.nickname = nickname;
+    	this.password = password;
+    	this.totalAnswer = totalAnswer;
+    	this.correctAnswer = correctAnswer;
+    	this.rating = rating;
+    	
+    }
+    
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public int getUserNo() {
+		return userNo;
+	}
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getTotalAnswer() {
+		return totalAnswer;
+	}
+	public void setTotalAnswer(int totalAnswer) {
+		this.totalAnswer = totalAnswer;
+	}
+	public int getCorrectAnswer() {
+		return correctAnswer;
+	}
+	public void setCorrectAnswer(int correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+	public float getRating() {
+		return rating;
+	}
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+    
+    
 } 
