6a8e89bd36ac5454165d0f7c11f11bbbf3c50ada
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 00:36:38 +0900

    ApplicationControllerを実装

--- quizoo/src/main/java/frame/ApplicationController.java
+++ quizoo/src/main/java/frame/ApplicationController.java
@@ -1,1 +1,10 @@
-public interface ApplicationController {} 
+package frame;
+
+import frame.context.RequestContext;
+import frame.context.ResponseContext;
+
+public interface ApplicationController {
+    public RequestContext getRequest(Object request);
+    public ResponseContext handleRequest(RequestContext request);
+    public void handleResponse(RequestContext request, ResponseContext response);
+}
