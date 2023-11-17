e01dcfef10f21614f7dd65df9284369c569f4d12
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 00:20:42 +0900

    RequestContextを実装

--- quizoo/src/main/java/frame/context/RequestContext.java
+++ quizoo/src/main/java/frame/context/RequestContext.java
@@ -1,1 +1,13 @@
-public interface RequestContext {} 
+package frame.context;
+
+public interface RequestContext {
+	void setAttribute(String key, Object value);
+
+	String[] getParameter(String key);
+
+	void setId(String userId);
+
+	String getId();
+
+	String getMessageBody();
+}
