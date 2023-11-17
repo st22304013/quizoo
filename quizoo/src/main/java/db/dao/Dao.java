a5034d6cdaecaa9a8cfbaf5fd5574cb94529189f
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-17 11:27:53 +0900

    接続、切断処理を修正

--- quizoo/src/main/java/db/dao/Dao.java
+++ quizoo/src/main/java/db/dao/Dao.java
@@ -13,44 +13,64 @@ import frame.exception.ResourceException;
 
 
 public abstract class Dao {
-	protected static Connection cn;
+	private Connection cn;
+	
 
 	public void connect() throws ResourceException {
 		
-		try {
-			
-			Context c;
-			String dataSource = "java:comp/env/jdbc/LoginTemplate";
-			c = new InitialContext();
-			DataSource ds = (DataSource)c.lookup(dataSource);
-			
-			
-			cn = ds.getConnection();
-			cn.setAutoCommit(false);
-			
-			System.out.println("コネクションプールからの取得に成功しました"+cn);
-			
-		} catch (NamingException | SQLException e) {
-			System.out.println("コネクションプールからの取得に失敗しました");
-			System.out.println("コネクションを再度取得します（プールを使用しない）");
-			
 			try {
-				Class.forName("com.mysql.cj.jdbc.Driver");
+//				----Tomcatからコネクションを取得----
+				Context c;
+				String dataSource = "java:comp/env/jdbc/LoginTemplate";
+				c = new InitialContext();
 				
-				cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizoo");
+				DataSource ds = (DataSource)c.lookup(dataSource);
 				
 				
+				cn = ds.getConnection();
+				cn.setAutoCommit(false);
 				
-			} catch (ClassNotFoundException | SQLException e1) {
-				throw new ResourceException(e.getMessage(), e);
+				System.out.println("コネクションプールからの取得に成功しました"+cn);
+				
+			} catch (NamingException | SQLException e) {
+//				---Tomcatからコネクションが取得できなかったとき---
+				
+				close(); //Tomcatで取得しようとしたコネクションを切断
+				
+				System.out.println("\u001B[101m コネクションプールからの取得に失敗しました\u001B[0m");
+				System.out.println("コネクションを再度取得します（プールを使用しない）");
+				
+				try {
+					
+					Class.forName("com.mysql.cj.jdbc.Driver"); //jdbcドライバの読み込み
+					
+					cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizoo","quizoo_app","app");
+					
+					System.out.println("コネクション\u001B[42m" + cn + "\u001B[0mが取得されました");
+					
+				} catch (ClassNotFoundException | SQLException e1) {
+					
+					
+					System.out.println("\u001B[101m 取得に失敗しました\u001B[0m");
+					throw new ResourceException(e.getMessage(), e);
+				}
 			}
+			
 		}
 		
 		
-	}
 
-	public void close() throws SQLException {
-		cn.close();
+	public void close() throws ResourceException{
+		try {
+			if(cn!=null) {
+				String cnStr = cn.toString();
+				cn.close();	
+				
+				System.out.println("コネクション\u001B[42m"+cnStr+"\u001B[0mがクローズされました");
+			}
+		} catch (SQLException e) {
+			throw new ResourceException(e.getMessage(), e);
+		}
 	}
 
 }
