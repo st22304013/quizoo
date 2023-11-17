9065be78c428ae5d99139dd94ef5f68926e258e2
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 01:11:05 +0900

    ResponseContextのメソッドをpublicに変更

--- quizoo/src/main/java/frame/context/ResponseContext.java
+++ quizoo/src/main/java/frame/context/ResponseContext.java
@@ -3,6 +3,6 @@ package frame.context;
 import java.io.PrintWriter;
 
 public interface ResponseContext {
-    void forward(String url, RequestContext requestContext);
-    PrintWriter getWrite();
+    public void forward(String url, RequestContext requestContext);
+    public PrintWriter getWrite();
 }
