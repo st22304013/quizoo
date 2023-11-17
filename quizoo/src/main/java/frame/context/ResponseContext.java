9789c88d5e5dcf070560e75b8509f13fd3fc7c9b
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 00:21:54 +0900

    ResponceContextを実装

--- quizoo/src/main/java/frame/context/ResponseContext.java
+++ quizoo/src/main/java/frame/context/ResponseContext.java
@@ -1,1 +1,8 @@
-public interface ResponseContext {} 
+package frame.context;
+
+import java.io.PrintWriter;
+
+public interface ResponseContext {
+    void forward(String url, RequestContext requestContext);
+    PrintWriter getWrite();
+}
