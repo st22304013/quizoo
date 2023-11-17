4677150262c9e54749e251c18eb493f48ac95e4e
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 01:10:37 +0900

    RequestContextのメソッドをpublicに編子

--- quizoo/src/main/java/frame/context/RequestContext.java
+++ quizoo/src/main/java/frame/context/RequestContext.java
@@ -1,13 +1,13 @@
 package frame.context;
 
 public interface RequestContext {
-	void setAttribute(String key, Object value);
+	public void setAttribute(String key, Object value);
 
-	String[] getParameter(String key);
+	public String[] getParameter(String key);
 
-	void setId(String userId);
+	public void setId(String userId);
 
-	String getId();
+	public String getId();
 
-	String getMessageBody();
+	public String getMessageBody();
 }
