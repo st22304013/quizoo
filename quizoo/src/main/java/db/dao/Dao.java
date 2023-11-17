6425ff594127aac69ca14541d26b39c5b018411c
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-17 10:05:03 +0900

    connectメソッドを作成

---
+++ quizoo/src/main/java/db/dao/Dao.java
@@ -0,0 +1,54 @@
+package db.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+
+public abstract class Dao {
+	protected static Connection cn;
+
+	public void connect() {
+		
+		try {
+			
+			Context c;
+			String dataSource = "java:comp/env/jdbc/LoginTemplate";
+			c = new InitialContext();
+			DataSource ds = (DataSource)c.lookup(dataSource);
+			
+			
+			cn = ds.getConnection();
+			cn.setAutoCommit(false);
+			
+			System.out.println("コネクションプールからの取得に成功しました"+cn);
+			
+		} catch (NamingException | SQLException e) {
+			System.out.println("コネクションプールからの取得に失敗しました");
+			System.out.println("コネクションを再度取得します（プールを使用しない）");
+			
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				
+				cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/quizoo");
+				
+				
+				
+			} catch (ClassNotFoundException | SQLException e1) {
+				
+			}
+		}
+		
+		
+	}
+
+	public void close() throws SQLException {
+		cn.close();
+	}
+
+}
