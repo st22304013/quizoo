43d2613bf94287d94383c2de63bac7b3c826d246
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 22:58:14 +0900

    Serviceクラスの実行を追加

--- quizoo/src/main/java/web/FrontServlet.java
+++ quizoo/src/main/java/web/FrontServlet.java
@@ -7,16 +7,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import frame.Service;
 import frame.ServiceFactory;
 import frame.exception.NotFoundException;
 import frame.exception.ResourceException;
 import web.context.HttpRequestContext;
+import web.context.HttpResponseContext;
 
 public class FrontServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		try {
-			ServiceFactory.getService(req.getServletContext().getResourceAsStream("WEB-INF/command-mapping.properties"),new HttpRequestContext(req));
+			Service service =  ServiceFactory.getService(req.getServletContext().getResourceAsStream("WEB-INF/command-mapping.properties"),new HttpRequestContext(req));
+			HttpRequestContext reqc = new HttpRequestContext(req);
+			HttpResponseContext resc = new HttpResponseContext(resp);
+			service.execute(reqc, resc);
 		} catch (ResourceException | NotFoundException e) {
 			e.printStackTrace();
 			throw new ServletException(e);
