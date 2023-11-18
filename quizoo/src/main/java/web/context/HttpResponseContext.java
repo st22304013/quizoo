9a48edd1a5b6a8019427cb00e6cb80d3a9780733
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 22:47:05 +0900

    HttpResponseContextを実装

--- quizoo/src/main/java/web/context/HttpResponseContext.java
+++ quizoo/src/main/java/web/context/HttpResponseContext.java
@@ -1,1 +1,36 @@
-public class HttpResponseContext {} 
+package web.context;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import frame.context.RequestContext;
+import frame.context.ResponseContext;
+import frame.exception.BadRequestException;
+
+public class HttpResponseContext implements ResponseContext{
+	
+	HttpServletResponse res = null;
+	
+	@Override
+	public void forward(String url, RequestContext req) throws BadRequestException {
+		HttpServletRequest sreq = ((HttpRequestContext)req).getRequest();
+		
+		try {
+			sreq.getRequestDispatcher(url).forward(sreq,res);
+		} catch (ServletException | IOException e) {
+			throw new BadRequestException(e.getMessage(), e);
+		}
+		
+	}
+
+	@Override
+	public PrintWriter getWrite() throws IOException {
+		return res.getWriter();
+	}
+	
+	
+} 
