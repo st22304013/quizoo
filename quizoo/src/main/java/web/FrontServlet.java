e1bfbd8d6df730e511716bddb44708fef35bc7bd
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 21:54:56 +0900

    FrontServletを実装

--- quizoo/src/main/java/web/FrontServlet.java
+++ quizoo/src/main/java/web/FrontServlet.java
@@ -7,10 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import frame.ServiceFactory;
+import frame.exception.NotFoundException;
+import frame.exception.ResourceException;
+import web.context.HttpRequestContext;
+
 public class FrontServlet extends HttpServlet{
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		try {
+			ServiceFactory.getService(req.getServletContext().getResourceAsStream("WEB-INF/command-mapping.properties"),new HttpRequestContext(req));
+		} catch (ResourceException | NotFoundException e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
 	}
 } 
 
