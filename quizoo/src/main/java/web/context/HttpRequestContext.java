b5dd945d9ed0ee814b7c18c222c61c2e83c714b9
sora-jbk <aj22304027@ga.ttc.ac.jp>
2023-11-18 21:54:21 +0900

    HttpRequestCOntextのgetTargetServiceKeyを実装

--- quizoo/src/main/java/web/context/HttpRequestContext.java
+++ quizoo/src/main/java/web/context/HttpRequestContext.java
@@ -1,1 +1,41 @@
-public class HttpRequestContext {} 
+package web.context;
+
+import javax.servlet.http.HttpServletRequest;
+
+import frame.context.RequestContext;
+
+public class HttpRequestContext implements RequestContext{
+	HttpServletRequest req = null;
+	public HttpRequestContext(HttpServletRequest req) {
+		this.req = req;
+	}
+	@Override
+	public void setAttribute(String key, Object value) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	@Override
+	public String[] getParameter(String key) {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	@Override
+	public void setId(String userId) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	}
+	@Override
+	public String getId() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	@Override
+	public String getMessageBody() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+	@Override
+	public String getTargetServiceKey() {
+		return req.getRequestURI().toString().replaceFirst(".","").replaceAll("/", ".");
+	}
+} 
