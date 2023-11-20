8b1b10068de68b71b54ba2630cfad4574fda0d9a
niko0816 <nt22304016@ga.ttc.ac.jp>
2023-11-20 16:43:23 +0900

    QuizDao修正しました

--- quizoo/src/main/java/db/dao/QuizDao.java
+++ quizoo/src/main/java/db/dao/QuizDao.java
@@ -5,7 +5,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class QuizDao extends Dao{	
-	public QuizBean selectQuiz(int quizId) {
+	public QuizBean selectQuiz(int quizId) throws ResourceException {
 		
 		PreparedStatement st = null;
 		ResultSet rs = null;
@@ -14,32 +14,31 @@ public class QuizDao extends Dao{
 		try {
 			connect();
 			
-			String sql = "SELECT * FROM quiz WHERE quiz_id = " + quizId; 
+			String sql = "SELECT * FROM quiz WHERE quiz_id = ?"; 
 			st = cn.prepareStatement(sql);
+			st.setInt(1, quizId);
 			rs = st.executeQuery();
 			
 			while(rs.next()) {
-				quizbean.setquizId(rs.getInt(1));
-				quizbean.setAuthorNo = rs.getInt(2);
-				quizbean.setAuthorNickname = rs.getString(3);
-				quizbean.setTitle = rs.getString(4);
-				quizbean.setQuestionCount = rs.getInt(5);
-				quizbean.setGenreNo = rs.getInt(6);
-				quizbean.setGenre = rs.getString(7);
-				quizbean.setExplanation = rs.getString(8);
-				quizbean.setCreateTime = rs.getString(9);
-				quizbean.setCorrectRate = rs.getFloat(10);
-				quizbean.setTotalParticipants = rs.getInt(11);	
-				
+				quizbean.setQuizId(rs.getInt(1));
+				quizbean.setAuthorNo(rs.getInt(2));
+				quizbean.setTitle(rs.getString(3));
+				quizbean.setQuestionCount(rs.getInt(4));
+				quizbean.setGenreNo(rs.getInt(5));
+				quizbean.setExplanation(rs.getString(6));
+				quizbean.setCreateTime(rs.getString(7));
+				quizbean.setCorrectRate(rs.getFloat(8));
+				quizbean.setTotalParticipants(rs.getInt(9));	
+			
 				cn.commit();
 			}
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
 		} catch(SQLException e) {
 			try {
 				cn.rollback();
 			} catch(SQLException e2) {
-				
+				throw new ResourceException(e2.getMessage(), e2);
 			}
 		} finally {
 			try {
@@ -50,7 +49,7 @@ public class QuizDao extends Dao{
 					st.close();
 				}
 			} catch(SQLException e2) {
-				
+				throw new ResourceException(e2.getMessage(), e2);
 			} finally {
 				close();
 			}
@@ -58,38 +57,36 @@ public class QuizDao extends Dao{
 		return quizbean;
 	}
 	
-	public void insertQuiz(QuizBean quiz) {
+	public void insertQuiz(QuizBean quiz) throws ResourceException {
 		PreparedStatement st = null;
 		
 		try {
 			connect();
             
-			String sql = "INSERT INTO quiz VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			String sql = "INSERT INTO quiz VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)";
 			st = cn.prepareStatement(sql);
 			
-			st.setInt(1, quiz.getquizId());
-			st.setInt(2, quiz.getAuthor_no());
-			st.setString(3, quiz.geAuthorNicknamet());
-			st.setString(4, quiz.getTitle());
-			st.setInt(5, quiz.getQuestionCount());
-			st.setInt(6, quiz.getGenreNo());
-			st.setString(7, quiz.getGenre());
-			st.setString(8, quiz.getExplanation());
-			st.setString(9, quiz.getCreateTime());
-			st.setFloat(10, quiz.getCorrectRate());
-			st.setInt(11, quiz.getTotalParticipants());
+			st.setInt(1, quiz.getQuizId());
+			st.setInt(2, quiz.getAuthorNo());
+			st.setString(3, quiz.getTitle());
+			st.setInt(4, quiz.getQuestionCount());
+			st.setInt(5, quiz.getGenreNo());
+			st.setString(6, quiz.getExplanation());
+			st.setString(7, quiz.getCreateTime());
+			st.setFloat(8, quiz.getCorrectRate());
+			st.setInt(9, quiz.getTotalParticipants());
 			
 			st.executeUpdate();
 			
 			cn.commit();
 	
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
         } catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
-                
+            	throw new ResourceException(e2.getMessage(), e2);
             }
         } finally {
             close();
@@ -97,26 +94,27 @@ public class QuizDao extends Dao{
 		
 	}
 	
-	public void deleteQuiz(int quizId) {
+	public void deleteQuiz(int quizId) throws ResourceException {
 		PreparedStatement st = null;
 		
 		try {
 			connect();
 			
-			String sql = "DELETE FROM quiz WHERE quizId = " + quizId;
+			String sql = "DELETE FROM quiz WHERE quiz_id = ?";
 			st = cn.prepareStatement(sql);
+			st.setInt(1, quizId);
 			
 			st.executeUpdate();
 			
 			cn.commit();
 			
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
         } catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
-                
+            	throw new ResourceException(e2.getMessage(), e2);
             }
         } finally {
         	close();
@@ -124,26 +122,28 @@ public class QuizDao extends Dao{
 		
 	}
 	
-	public void updateTitle(int quizId, int title) {
+	public void updateTitle(int quizId, String title) throws ResourceException {
 		PreparedStatement st = null;
 		
 		try {
 			connect();
 			
-			String sql = "UPDATE quiz SET title = " +  title + " WHERE quizId = " + quizId;
+			String sql = "UPDATE quiz SET title = ? WHERE quiz_id = ?";
 			st = cn.prepareStatement(sql);
+			st.setString(1, title);
+			st.setInt(2, quizId);
 			
 			st.executeUpdate();
 			
 			cn.commit();
 			
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
         } catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
-                
+            	throw new ResourceException(e2.getMessage(), e2);
             }
         } finally {
         	close();
@@ -151,52 +151,56 @@ public class QuizDao extends Dao{
 		
 	}
 	
-	public void updateGenre(int quizId, int genreNo) {
+	public void updateGenre(int quizId, int genreNo) throws ResourceException{
 		PreparedStatement st = null;
 		
 		try {
 			connect();
 			
-			String sql = "UPDATE quiz SET genreNo = " +  genreNo + " WHERE quizId = " + quizId;
+			String sql = "UPDATE quiz SET genre_no = ? WHERE quiz_id = ?";
 			st = cn.prepareStatement(sql);
+			st.setInt(1, genreNo);
+			st.setInt(2, quizId);
 			
 			st.executeUpdate();
 			
 			cn.commit();
 			
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
         } catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
-                
+            	throw new ResourceException(e2.getMessage(), e2);
             }
         } finally {
         	close();
         }
 	}
 	
-	public void updateExplanation(int quizId, String explanation) {
+	public void updateExplanation(int quizId, String explanation) throws ResourceException {
 		PreparedStatement st = null;
 		
 		try {
 			connect();
 			
-			String sql = "UPDATE quiz SET explanation = " +  explanation + " WHERE quizId = " + quizId;
+			String sql = "UPDATE quiz SET explanation = ? WHERE quiz_id = ?";
 			st = cn.prepareStatement(sql);
+			st.setString(1, explanation);
+			st.setInt(2, quizId);
 			
 			st.executeUpdate();
 			
 			cn.commit();
 			
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
         } catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
-                
+            	throw new ResourceException(e2.getMessage(), e2);
             }
         } finally {
         	close();
@@ -204,26 +208,28 @@ public class QuizDao extends Dao{
 		
 	}
 	
-	public void updateRateAndTotalPaticipants(int quizId, int score) {
+	public void updateRateAndTotalPaticipants(int quizId, int score) throws ResourceException {
 		PreparedStatement st = null;
 		
 		try {
 			connect();
 			
-			String sql = "UPDATE quiz SET  score = " + score + " WHERE quizId = " + quizId;
+			String sql = "UPDATE quiz SET score = ? WHERE quiz_id = ?";
 			st = cn.prepareStatement(sql);
+			st.setInt(1, score);
+			st.setInt(2, quizId);
 			
 			st.executeUpdate();
 			
 			cn.commit();
 			
 		} catch(ClassNotFoundException e) {
-			
+			throw new ResourceException(e.getMessage(), e);
         } catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
-                
+            	throw new ResourceException(e2.getMessage(), e2);
             }
         } finally {
         	close();
