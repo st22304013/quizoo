52569fadcc21331229ceb127bbe61e93117c2a34
komamura <ek22304009@ga.ttc.ac.jp>
2023-11-22 15:26:55 +0900

    完成しました。ハッシュ化はしてません

--- quizoo/src/main/java/db/dao/UserInfoDao.java
+++ quizoo/src/main/java/db/dao/UserInfoDao.java
@@ -4,14 +4,12 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.naming.NamingException;
-
 import db.bean.UserInfoBean;
 import frame.exception.ResourceException;
 
 public class UserInfoDao extends Dao {
 
-	public UserInfoBean selectUser(String userId) throws ResourceException {
+	public UserInfoBean selectUser(String user_id) throws ResourceException {
 
 		PreparedStatement st = null;
 		ResultSet rs = null;
@@ -20,30 +18,35 @@ public class UserInfoDao extends Dao {
 		try {
 			connect();
 
-			String sql = "SELECT * FROM userinfo WHERE quiz_id = ?"; 
+			String sql = "SELECT * FROM userinfo JOIN nickname USING(user_no) WHERE user_id = ?";
 			st = cn.prepareStatement(sql);
-			st.setString(1, userId);
+			st.setString(1, user_id);
 			rs = st.executeQuery();
+			if (rs.next()) {
+				userbean.setUserId(rs.getString("user_id"));
+				userbean.setUserNo(rs.getInt("user_no"));
+				userbean.setNickname(rs.getString("nickname"));
+				userbean.setPassword(rs.getString("password"));
+				userbean.setTotalAnswer(rs.getInt("total_answer"));
+				userbean.setCorrectAnswer(rs.getInt("correct_answer"));
+				userbean.setRating(rs.getFloat("rating"));
+			}
 
-
-				cn.commit();
-		} catch(ClassNotFoundException e) {
-			throw new ResourceException(e.getMessage(), e);
-		} catch(SQLException e) {
+		} catch (SQLException e) {
 			try {
 				cn.rollback();
-			} catch(SQLException e2) {
+			} catch (SQLException e2) {
 				throw new ResourceException(e2.getMessage(), e2);
 			}
 		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				if(st != null) {
+				if (st != null) {
 					st.close();
 				}
-			} catch(SQLException e2) {
+			} catch (SQLException e2) {
 				throw new ResourceException(e2.getMessage(), e2);
 			} finally {
 				close();
@@ -52,21 +55,83 @@ public class UserInfoDao extends Dao {
 		return userbean;
 	}
 
+	public void InsertUser(UserInfoBean user) throws ResourceException {
+		PreparedStatement st = null;
 
-public void InsertUser(UserInfoBean user) throws ResourceException{
-	PreparedStatement st = null;
-	
-	try {
+		try {
 			connect();
-			
-			String sql =" INSERT INTO userinfo VALUES(?,?,?,?,?,?,?)";
+
+			String sql = " INSERT INTO userinfo VALUES(?,?,?,?,?,?,?)";
 			st = cn.prepareStatement(sql);
-			
-			st.setString(1,user.getUserId());
-			st.setInt(2,user.getUserNo());
-			st.setString(3,user.getNickname());
-			st.setString(4,user.getPassword());
-			st.setInt(5,user.getTotalAnswer());
-			st.setInt(6,user.getCorrectAnswer());
-			st.setFloat(7,user.getRating());
+
+			st.setString(1, user.getUserId());
+			st.setInt(2, user.getUserNo());
+			st.setString(3, user.getNickname());
+			st.setString(4, user.getPassword());
+			st.setInt(5, user.getTotalAnswer());
+			st.setInt(6, user.getCorrectAnswer());
+			st.setFloat(7, user.getRating());
+		} catch (SQLException e) {
+			throw new ResourceException(e.getMessage(), e);
+		}
+
+	}
+
+	public void deleteUser(String user_id) throws ResourceException {
+		PreparedStatement st = null;
+		try {
+			connect();
+
+			String sql = "DELETE FROM userinfo WHERE user_id = ?";
+			st = cn.prepareStatement(sql);
+			st.setString(1, user_id);
+
+			st.executeUpdate();
+			cn.commit();
+		} catch (SQLException e) {
+			try {
+				cn.rollback();
+			} catch (SQLException e2) {
+				throw new ResourceException(e2.getMessage(), e2);
+			}
+		} finally {
+			close();
+		}
+	}
+
+	public void updatePassword(String user_id, String password) throws ResourceException {
+		PreparedStatement st = null;
+		try {
+			connect();
+
+			String sql = "UPDATE userinfo SET password =? WHERE user_id =?";
+			st = cn.prepareStatement(sql);
+			st.setString(1, password);
+			st.setString(2, user_id);
+
+			st.executeUpdate();
+
+			cn.commit();
+		} catch (SQLException e) {
+			throw new ResourceException(e.getMessage(), e);
+		} finally {
+			close();
+		}
+	}
+
+	public void updateScore(int answered, int correct) throws ResourceException {
+		PreparedStatement st = null;
+		try {
+			connect();
+			String sql = "UPDATE userinfo SET total_answered = total_answered + ?,correct_ansewerd = correct_answered + ?";
+			st = cn.prepareStatement(sql);
+			st.setInt(1, answered);
+			st.setInt(2, correct);
+			st.executeUpdate();
+
+			cn.commit();
+		} catch (SQLException e) {
+			throw new ResourceException(e.getMessage(), e);
+		}
+	}
 }
