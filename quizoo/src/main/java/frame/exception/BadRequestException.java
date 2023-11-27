af0fd96b9caa7fbb83428c9dc9812789f877996d
komamura <ek22304009@ga.ttc.ac.jp>
2023-11-27 11:22:46 +0900

    コンストラクタを追加

--- quizoo/src/main/java/frame/exception/BadRequestException.java
+++ quizoo/src/main/java/frame/exception/BadRequestException.java
@@ -4,4 +4,8 @@ public class BadRequestException extends Exception{
 	public BadRequestException(String msg,Exception e) {
 		super(msg,e);
 	}
+
+	public BadRequestException(String msg) {
+		super(msg);
+	}
 } 
