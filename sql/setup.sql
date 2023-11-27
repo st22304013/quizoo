9ef2aebbe2b4c97831313de57688c924a5f0634a
wataru-51 <kw22304025@ga.ttc.ac.jp>
2023-11-27 15:41:01 +0900

    メイン動かしたいから

--- sql/setup.sql
+++ sql/setup.sql
@@ -100,7 +100,9 @@ CREATE TRIGGER set_question_count
 BEFORE INSERT ON answerhistory
 FOR EACH ROW
 BEGIN
-    SET NEW.question_count = (SELECT question_count FROM quiz WHERE quiz_id = NEW.quiz_id);
+    INSERT INTO answerhistory (question_count) SELECT question_count FROM quiz WHERE quiz_id = quiz_id;
 END;
 //
 DELIMITER ;
+
+insert into answerhistory(user_no, quiz_id, answered_time, correct_count) values(2,2,now(),1);
