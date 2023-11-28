09c3e6c92654a0b96969226d3657db2d63221874
wataru-51 <kw22304025@ga.ttc.ac.jp>
2023-11-28 14:14:18 +0900

    トリガーのテスト

--- sql/test.sql
+++ sql/test.sql
@@ -1,5 +1,7 @@
 /*set_questin_countをテストする文　question_countに入る数は何でもいい。自動的にquiz表のquestion_countに変わる*/
-insert into answerhistory(user_no, quiz_id, answered_time, question_count, correct_count) values(3,2,now(),1,1);
+insert into answerhistory(user_no, quiz_id, answered_time, correct_count) values(5,1,now(),1);
+
+
 
 /*calculate_rating*をテストする文*/
 update userinfo set total_answer=2, correct_answer=2 where user_no=5;
