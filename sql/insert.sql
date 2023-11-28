cf90832cb73dd9cb213b3e1ae5623c3d38bd926b
wataru-51 <kw22304025@ga.ttc.ac.jp>
2023-11-28 14:12:15 +0900

    テスト用のデータ改

--- sql/insert.sql
+++ sql/insert.sql
@@ -3,7 +3,8 @@ INSERT INTO userinfo (user_id, password, total_answer, correct_answer, rating) V
 ('watoru5151@gmail.com', 'password1', 2, 1, 0.5),
 ('jibiki@gmail.com', 'password2', 3, 0, 0),
 ('eiya@gmail.com', 'password3', 15, 15, 15),
-('niko@gmail.com', 'password4', 100, 80, 64);
+('niko@gmail.com', 'password4', 100, 80, 64),
+('tester', 'test', 0, 0, 0);
 
 
 /* nickname テーブルへのデータ挿入 */
@@ -31,10 +32,10 @@ INSERT INTO genre (genre_no, genre_title) VALUES
 
 
 /* quiz テーブルへのデータ挿入 */
-INSERT INTO quiz (author_no, title, question_count, genre_no, explanation, create_time, correct_rate, total_participants) VALUES
-(1, '亘クイズ', 3, 5, 'おれに関するクイズやで', now(0), 1, 1),
-(2, 'びきクイズ', 1, 7, NULL, now(0), 1, 1),
-(1, '動物クイズ', 2, 9, 'いろんな動物のクイズ', now(0), 1, 2);
+INSERT INTO quiz (author_no, title, question_count, genre_no, explanation, create_time, total_participants) VALUES
+(1, '亘クイズ', 3, 5, 'おれに関するクイズやで', now(0), 1),
+(2, 'びきクイズ', 1, 7, NULL, now(0), 1),
+(1, '動物クイズ', 2, 9, 'いろんな動物のクイズ', now(0), 2);
 
 
 /* question テーブルへのデータ挿入 */
@@ -42,9 +43,9 @@ INSERT INTO question (quiz_id, question_id, question, choise_1, choise_2, choise
 (1, 1, '亘の好きな食べ物は？', 'ラーメン', '寿司', 'カツオ', 'プリン', b'1100'),
 (1, 2, '亘の嫌いな食べ物は？', 'カツオ', 'プリン', 'カリフラワー', '大根', b'0010'),
 (1, 3, '亘の身長は？', '169', '170', '171', '172', b'0010'),
-(1, 4, '地引の居住地は？', '神栖', '筑波', '大洗', '取手', b'0001'),
-(1, 5, '「く」から始まる幸せな顔をした動物は？', 'クシナダ', 'クオッカ', 'クジラ', 'バナナ', b'0100'),
-(1, 6, 'オランウータンの握力は何キロ？', '100', '150', '200', '250', b'0010');
+(2, 4, '地引の居住地は？', '神栖', '筑波', '大洗', '取手', b'0001'),
+(3, 5, '「く」から始まる幸せな顔をした動物は？', 'クシナダ', 'クオッカ', 'クジラ', 'バナナ', b'0100'),
+(3, 6, 'オランウータンの握力は何キロ？', '100', '150', '200', '250', b'0010');
 
 
 /* answerhistory テーブルへのデータ挿入 */
