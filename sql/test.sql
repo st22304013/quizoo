e243816a860d4776b1516fda70ee6cddee4b496d
wataru-51 <kw22304025@ga.ttc.ac.jp>
2023-11-29 15:22:29 +0900

    トリガーテスト

--- sql/test.sql
+++ sql/test.sql
@@ -2,7 +2,6 @@
 insert into answerhistory(user_no, quiz_id, answered_time, correct_count) values(5,1,now(),3);
 
 
-
 /*calculate_rating*‚ðƒeƒXƒg‚·‚é•¶*/
 update userinfo set total_answer=3, correct_answer=2 where user_no=5;
 
