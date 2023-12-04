85da3b00fab8055e5a03d7e5c7985a8159484adb
wataru-51 <kw22304025@ga.ttc.ac.jp>
2023-12-04 15:45:47 +0900

    Jsonのテスト用コンテキスト　レスポンス

--- quizoo/src/main/java/web/context/TestResponseContext.java
+++ quizoo/src/main/java/web/context/TestResponseContext.java
@@ -3,12 +3,16 @@ package web.context;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import frame.context.RequestContext;
 import frame.context.ResponseContext;
 import frame.exception.BadRequestException;
 
 public class TestResponseContext implements ResponseContext {
+	public void forward(String url, RequestContext requestContext) throws  BadRequestException {
+	}
+	public PrintWriter getWrite() throws IOException {
+		return null;
+	}
+	 public void redirect(String url) throws IOException {
+	 }
+}
