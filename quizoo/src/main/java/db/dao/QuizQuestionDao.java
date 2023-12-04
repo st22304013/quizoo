c97f765a35279f8adfcd685fea1214dec9ca4270
st22304013 <st22304013@ttc.ac.jp>
2023-12-04 16:12:34 +0900

    QuestionBeanのjedgeの型変更に合わせてselectQuizWithQuestionメソッドのwhile文の中のgetByteの部分を修正し、あとinsertQuestionメソッドを作成した

--- quizoo/src/main/java/db/dao/QuizQuestionDao.java
+++ quizoo/src/main/java/db/dao/QuizQuestionDao.java
@@ -5,6 +5,11 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import db.bean.QuestionBean;
+import db.bean.QuizBean;
+import db.bean.QuizQuestionBean;
+import frame.exception.ResourceException;
+
 public class QuizQuestionDao extends Dao{
 	public QuizQuestionBean selectQuizWithQuestion(int quizid) throws ResourceException{
 		
@@ -46,26 +51,27 @@ public class QuizQuestionDao extends Dao{
 			while(rs.next()) {
 				QuestionBean questionBean = new QuestionBean();
 				
-				questionBean.setQuizId(rs.getInt("quiz_id"));
-				questionBean.setQuestionId(rs.getInt("question_id"));
+				questionBean.setQuiz_id(rs.getInt("quiz_id"));
+				questionBean.setQuestion_id(rs.getInt("question_id"));
 				questionBean.setQuestion(rs.getString("question"));
-				questionBean.setChoice1(rs.getString("choice_1"));
-				questionBean.setChoice2(rs.getString("choice_2"));
-				questionBean.setChoice3(rs.getString("choice_3"));
-				questionBean.setChoice4(rs.getString("choice_4"));
-				questionBean.setJudge(rs.getByte("judge"));
+				questionBean.setChoice_1(rs.getString("choice_1"));
+				questionBean.setChoice_1(rs.getString("choice_2"));
+				questionBean.setChoice_1(rs.getString("choice_3"));
+				questionBean.setChoice_1(rs.getString("choice_4"));
+				byte judgeByte = rs.getByte("judge");
+	            // ビット列をboolean[]に変換するメソッドを呼び出してセット
+	            boolean[] judgeArray = byteToBooleanArray(judgeByte);
+	            questionBean.setJudge(judgeArray);
 				
 				questionList.add(questionBean);
 				
 			}
 			
-			quizQuestionBean.setQuestions(questionList);
+			quizQuestionBean.setQuestion(questionList);
 			
 			cn.commit();
 			
-		} catch(ClassNotFoundException e) {
-            throw new ResourceException(e.getMessage(), e);
-        } catch(SQLException e) {
+		} catch(SQLException e) {
             try{
                 cn.rollback();
             } catch(SQLException e2) {
@@ -87,5 +93,60 @@ public class QuizQuestionDao extends Dao{
             }
         }
 		return quizQuestionBean;
-	} 
+	}
+	
+	public void insertQuestion(QuestionBean question) throws ResourceException {
+	    PreparedStatement st = null;
+
+	    try {
+	        connect();
+
+	        String sql = "INSERT INTO question (quiz_id, question, choice_1, choice_2, choice_3, choice_4, judge) " +
+	                     "VALUES (?, ?, ?, ?, ?, ?, ?)";
+	        st = cn.prepareStatement(sql);
+
+	        st.setInt(1, question.getQuiz_id());
+	        st.setString(2, question.getQuestion());
+	        st.setString(3, question.getChoice_1());
+	        st.setString(4, question.getChoice_2());
+	        st.setString(5, question.getChoice_3());
+	        st.setString(6, question.getChoice_4());
+	        // boolean[]からビット文字列に変換してセット
+            String bitString = booleanArrayToBitString(question.getJudge());
+            st.setString(7, bitString);
+
+	        st.executeUpdate();
+
+	        cn.commit();
+
+	    } catch (SQLException e) {
+	        try {
+	            cn.rollback();
+	        } catch (SQLException e2) {
+	            throw new ResourceException(e2.getMessage(), e2);
+	        }
+	        throw new ResourceException(e.getMessage(), e);
+	    } finally {
+	        close();
+	    }
+	}
+	
+	// バイトをboolean[]に変換するメソッド
+    private boolean[] byteToBooleanArray(byte b) {
+        boolean[] result = new boolean[8];
+        for (int i = 0; i < 8; i++) {
+            result[i] = (b & (1 << i)) != 0;
+        }
+        return result;
+    }
+	
+	// boolean[]をビット文字列に変換するメソッド
+    private String booleanArrayToBitString(boolean[] boolArray) {
+        StringBuilder builder = new StringBuilder();
+        for (boolean b : boolArray) {
+            builder.append(b ? "1" : "0");
+        }
+        return builder.toString();
+    }
+
 }
