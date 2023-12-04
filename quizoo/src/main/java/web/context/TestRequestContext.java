5a3a2cf409bc2b48fc9f0e8c9469f385837e8c5f
wataru-51 <kw22304025@ga.ttc.ac.jp>
2023-12-04 14:48:19 +0900

    Jsonのテスト用コンテキスト　リクエスト

---
+++ quizoo/src/main/java/web/context/TestRequestContext.java
@@ -0,0 +1,30 @@
+package web.context;
+
+import db.bean.UserInfoBean;
+import frame.exception.ResourceException;
+
+public class TestRequestContext {
+	public void setAttribute(String key, Object value) {
+		
+	}
+
+	public String[] getParameter(String key) {
+		return null;
+	}
+
+	public void setUser(UserInfoBean user) {
+		
+	}
+
+	public UserInfoBean getUser() {
+		return null;
+	}
+
+	public String getMessageBody() throws ResourceException {
+		return 
+	}
+	
+	public String getTargetServiceKey() {
+		return null;
+	}
+}
