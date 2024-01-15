c4caa4e92e2d5e47468d283a8a04d7e2caec3322
sora-jbk <aj22304027@ga.ttc.ac.jp>
2024-01-15 14:52:38 +0900

    infomationをリンクの上レイヤに移動

--- quizoo/src/main/webapp/js/index.js
+++ quizoo/src/main/webapp/js/index.js
@@ -54,35 +54,28 @@ async function quizlistFactory(quizList){
 
         box = document.createElement('div');
         box.setAttribute('class','quiz');
-        link = document.createElement('a');
-        link.setAttribute('href','answer?quiz_id='+quiz['quizId']);
-        link.setAttribute('class','answer-link')
-        box.appendChild(link);
-
+        
         row = document.createElement('div');
         row.setAttribute('class','width row');
         title = document.createElement('div');
         title.setAttribute('class','title col');
         title.innerText = quiz['title'];
         row.appendChild(title);
-
+        
         explanation = document.createElement('div');
         explanation.setAttribute('class','d-flex align-items-center col');
         explanation.innerText = quiz['explanation'];
-
+        
         row.appendChild(explanation);
-
+        
         info = document.createElement('div');
         info.setAttribute('class','information text-right');
-
-        author = document.createElement('a');
-        author.setAttribute('class','author');
-        author.innerText = quiz['nickname'];
+        
         
         create_time = document.createElement('a');
         create_time.setAttribute('class','create_time');
         create_time.innerText = quiz['createTime'];
-
+        
         genre = document.createElement('a');
         genre.setAttribute('href',quiz['genre_no']);
         genre.innerText = quiz['genre'];
@@ -90,15 +83,24 @@ async function quizlistFactory(quizList){
         ratio = document.createElement('a');
         ratio.setAttribute('class','raito');
         ratio.innerText = ' ' + (parseFloat(quiz['correctRate'])/parseFloat(quiz['questionCount'])).toFixed(2);
-
-
+        
+        author = document.createElement('a');
+        author.setAttribute('class','author');
+        author.innerText = quiz['nickname'];
+        
         info.appendChild(author);
         info.appendChild(create_time);
         info.appendChild(genre);
         info.appendChild(ratio);    
-
-
+        
+        
         box.appendChild(row);
+
+        link = document.createElement('a');
+        link.setAttribute('href','answer?quiz_id='+quiz['quizId']);
+        link.setAttribute('class','answer-link')
+        box.appendChild(link);
+        
         box.append(info);
 
         list.appendChild(box);
