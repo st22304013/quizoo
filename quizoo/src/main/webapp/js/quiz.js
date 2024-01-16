960ffc435d17669fb66bc4dd270edfcc31743621
sora-jbk <aj22304027@ga.ttc.ac.jp>
2024-01-16 10:47:14 +0900

    quizquestionにfetchでリクエスト

--- quizoo/src/main/webapp/js/quiz.js
+++ quizoo/src/main/webapp/js/quiz.js
@@ -8,6 +8,24 @@ let quiz_id;
 window.addEventListener('load',function(){
     quizlist = this.document.querySelector('#quizlist');
 
-    quizId = window.history.state;
-    console.log(quizId);
+    // quiz_id‚ðƒNƒGƒŠ•¶Žš—ñ‚©‚çŽæ“¾
+    var currentURL = new URL(this.window.location.href);
+    var params = currentURL.searchParams;
+    quiz_id = params.get('quiz_id');
+
+    loadQuiz(quiz_id)
 })
+
+
+async function loadQuiz(quiz_id){
+    var result = await fetch('/quizoo/quizquestion?quiz_id='+quiz_id);
+    if(result.ok){
+        quizQuestionJson = result.json();
+    }else{
+        // redirect('/quizoo/error');
+    }
+}
+
+function redirect(url){
+    location.href = url;
+}
