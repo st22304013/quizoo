35e002e3c21284c45ba00198915a07f3aa24ae09
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-19 15:41:25 +0900

    graph.js追加

---
+++ quizoo/src/main/webapp/js/graph.js
@@ -0,0 +1,47 @@
+
+
+let historyList;
+window.addEventListener('load', async () => {
+    historyList = await fetch("/quizoo/answerhistory", {
+        credentials: "include"
+    });
+    historyList = await historyList.json();
+});
+
+const config = {
+  type: 'line',
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart'
+      }
+    }
+  },
+};
+
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.red,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+    },
+    {
+      label: 'Dataset 2',
+      data: Utils.numbers(NUMBER_CFG),
+      borderColor: Utils.CHART_COLORS.blue,
+      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+    }
+  ]
+};
+
+
