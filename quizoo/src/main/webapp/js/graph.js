cad5d3d1ffdb8142a53bd6fe6103affaf0ae8379
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-23 15:18:44 +0900

    canvasのidとscriptタグの追加

--- quizoo/src/main/webapp/js/graph.js
+++ quizoo/src/main/webapp/js/graph.js
@@ -1,47 +1,64 @@
 
+console.log("start");
+
+
 
 let historyList;
 window.addEventListener('load', async () => {
-    historyList = await fetch("/quizoo/answerhistory", {
-        credentials: "include"
-    });
-    historyList = await historyList.json();
+	historyList = await fetch("/quizoo/answerhistory", {
+		credentials: "include"
+	});
+	historyList = await historyList.json();
+	console.log(historyList);
+	
+	// グラフの初期化処理
+	document.addEventListener('DOMContentLoaded', function () {
+		const ctx = document.getElementById('chart').getContext('2d');
+		const myChart = new Chart(ctx, config);
+	});
+
+	data.labels = historyList.quizId;
+
 });
 
 const config = {
-  type: 'line',
-  data: data,
-  options: {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top',
-      },
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart'
-      }
-    }
-  },
+	type: 'line',
+	data: {
+		labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
+		datasets: [
+			{
+				label: 'score',
+				data: [10, 20, 30, 50],
+				borderColor: 'rgba(255, 99, 132, 1)',
+				backgroundColor: 'rgba(255, 99, 132, 0.2)',
+			},	          // ... 追加の datasets
+		]
+	},
+	options: {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: 'グラフ'
+			}
+		}
+	}
 };
 
 
 const data = {
-  labels: labels,
-  datasets: [
-    {
-      label: 'Dataset 1',
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.red,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-    },
-    {
-      label: 'Dataset 2',
-      data: Utils.numbers(NUMBER_CFG),
-      borderColor: Utils.CHART_COLORS.blue,
-      backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
-    }
-  ]
+	labels: null,
+	datasets: [
+		{
+			label: 'score',
+			data: [50, 20, 30, 40],
+			borderColor: 'rgba(0,0,0)',
+			backgroundColor: 'rgba(255, 99, 132, 0.2)',
+		},
+	]
 };
 
-
+console.log("end");
