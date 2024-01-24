aaa57829ffe8471ffc718a22531238dd7651f7ec
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-24 13:40:31 +0900

    グラフは表示されます。縦軸がクイズひとまとまりの正解率で、横軸がそのクイズの問題数です。

--- quizoo/src/main/webapp/js/graph.js
+++ quizoo/src/main/webapp/js/graph.js
@@ -1,8 +1,4 @@
 
-console.log("start");
-
-
-
 let historyList;
 window.addEventListener('load', async () => {
 	historyList = await fetch("/quizoo/answerhistory", {
@@ -11,24 +7,29 @@ window.addEventListener('load', async () => {
 	historyList = await historyList.json();
 	console.log(historyList);
 	
-	// グラフの初期化処理
-	document.addEventListener('DOMContentLoaded', function () {
-		const ctx = document.getElementById('chart').getContext('2d');
-		const myChart = new Chart(ctx, config);
-	});
+	for(var i = 0; i < historyList.length; i++) {
+		var correctCount = historyList[i].correctCount;
+		var questionCount = historyList[i].questionCount;
 
-	data.labels = historyList.quizId;
+		var rate = correctCount / questionCount;
+		console.log(rate);
 
+		config.data.datasets[0].data.push(rate);
+		config.data.labels.push(correctCount);
+	}
+
+	const ctx = document.getElementById('chart').getContext('2d');
+	const myChart = new Chart(ctx, config);
 });
 
-const config = {
+let config = {
 	type: 'line',
 	data: {
-		labels: ["Label 1", "Label 2", "Label 3", "Label 4"],
+		labels: [],
 		datasets: [
 			{
 				label: 'score',
-				data: [10, 20, 30, 50],
+				data: [],
 				borderColor: 'rgba(255, 99, 132, 1)',
 				backgroundColor: 'rgba(255, 99, 132, 0.2)',
 			},	          // ... 追加の datasets
@@ -49,16 +50,3 @@ const config = {
 };
 
 
-const data = {
-	labels: null,
-	datasets: [
-		{
-			label: 'score',
-			data: [50, 20, 30, 40],
-			borderColor: 'rgba(0,0,0)',
-			backgroundColor: 'rgba(255, 99, 132, 0.2)',
-		},
-	]
-};
-
-console.log("end");
