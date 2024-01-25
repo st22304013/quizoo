3f2501b630bc6e5eec9253a0970e688c6a92436a
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-25 14:22:55 +0900

    二個のグラフを出せた

--- quizoo/src/main/webapp/js/graph.js
+++ quizoo/src/main/webapp/js/graph.js
@@ -8,23 +8,29 @@ window.addEventListener('load', async () => {
 	console.log(historyList);
 	let correctCount = 0;
 	let questionCount = 0;
+	// 正解率をだすfor文
 	for(var i = 0; i < historyList.length; i++) {
 		
 		correctCount += historyList[i].correctCount;
 		questionCount += historyList[i].questionCount;
-
 		var quizCount = i;
 
-		var rate = (correctCount / questionCount) * 100;
-
-		config.data.datasets[0].data.push(rate);
+		var correctRate = (correctCount / questionCount) * 100;
+		config.data.datasets[0].data.push(correctRate);
 		config.data.labels.push(quizCount);
-	}
 
+		var rate = (correctCount * correctCount) / questionCount; 
+		config2.data.datasets[0].data.push(rate);
+		config2.data.labels.push(quizCount);
+		console.log(rate);
+	}
+	// スコアのChart 
 	const ctx = document.getElementById('chart').getContext('2d');
 	const myChart = new Chart(ctx, config);
 
-
+	// レートのChart 
+	const ctx2 = document.getElementById('chart2').getContext('2d');
+	const myChart2 = new Chart(ctx2, config2);
 
 });
 
@@ -46,6 +52,9 @@ let config = {
 		plugins: {
 			legend: {
 				position: 'top',
+				labels: {
+					usePointStyle: true,
+				},
 			},
 			title: {
 				display: true,
@@ -78,4 +87,56 @@ let config = {
 	}
 };
 
+
+let config2 = {
+	type: 'line',
+	data: {
+		labels: [],
+		datasets: [
+			{
+				label: 'レーティング',
+				data: [],
+				borderColor: 'rgba(255, 99, 132, 1)',
+				backgroundColor: 'rgba(255, 99, 132, 0.2)',
+			},
+		]
+	},
+	options: {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+				labels: {
+					usePointStyle: true,
+				},
+			},
+			title: {
+				display: true,
+				text: 'グラフ'
+			}
+		},
+		scales: {
+			x: {
+				min:0,
+				stepSize: 1,
+				title: {
+					display: true,
+					text: '回答数',
+				},
+			},
+			y: {				   
+				min:0,
+				stepSize: 1,
+				title: {
+					display: true,
+					text: 'レート',
+					padding: {
+						top: 0,
+						bottom: 0,
+					},
+				},	
+			}
+		}
+	}
+};
 
