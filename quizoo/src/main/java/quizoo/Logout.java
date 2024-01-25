0420cd63d6d3374398aa34a8ec335faacb425073
sora-jbk <aj22304027@ga.ttc.ac.jp>
2024-01-25 13:54:09 +0900

    ログアウトのServiceクラスを作成

---
+++ quizoo/src/main/java/quizoo/Logout.java
@@ -0,0 +1,21 @@
+package quizoo;
+
+import java.io.IOException;
+
+import frame.Service;
+import frame.context.RequestContext;
+import frame.context.ResponseContext;
+import frame.exception.BadRequestException;
+import frame.exception.NotFoundException;
+import frame.exception.ResourceException;
+
+public class Logout extends Service {
+
+	@Override
+	public void execute(RequestContext req, ResponseContext res)
+			throws IOException, ResourceException, BadRequestException, NotFoundException {
+		
+
+	}
+
+}
