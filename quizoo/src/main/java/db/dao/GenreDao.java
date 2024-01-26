2d39d3baf9de8c0e6a5d1ab8bf2c4ea6752aca53
st22304013 <st22304013@ttc.ac.jp>
2024-01-26 13:47:05 +0900

    GenreDaoの実装を始めた

---
+++ quizoo/src/main/java/db/dao/GenreDao.java
@@ -0,0 +1,11 @@
+package db.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import db.bean.QuizBean;
+import frame.exception.ResourceException;
+
+public class GenreDao extends Dao {
+	
+}
