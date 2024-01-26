7cbc7753ae11f20f83ead78243355ee859532c5f
st22304013 <st22304013@ttc.ac.jp>
2024-01-26 14:59:54 +0900

    GenreBeanひとまず完成した

--- quizoo/src/main/java/db/bean/GenreBean.java
+++ quizoo/src/main/java/db/bean/GenreBean.java
@@ -1,5 +1,31 @@
 package db.bean;
 
-public class GenreBean {
+import java.io.Serializable;
 
+public class GenreBean implements Serializable {
+	private int genreNo;
+	private String genre;
+	
+	public GenreBean() {}
+	
+	public GenreBean(int genreNo, String genre) {
+		this.setGenreNo(genreNo);
+		this.setGenre(genre);
+	}
+
+	public int getGenreNo() {
+		return genreNo;
+	}
+
+	public void setGenreNo(int genreNo) {
+		this.genreNo = genreNo;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
 }
