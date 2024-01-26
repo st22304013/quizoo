a27630bef6fa82e9d7a7643a272d152ed6f9eced
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-26 16:12:01 +0900

    プロフィール編集画面のニックネーム欄にもともとのニックネームを入れるためのjs

--- quizoo/src/main/webapp/js/profile.js
+++ quizoo/src/main/webapp/js/profile.js
@@ -1,3 +1,19 @@
 /**
  * 
  */
+let nicknameList;
+window.addEventListener("load", async () =>{
+    nicknameList = await fetch("/quizoo/nickname",{
+        credentials: "include"
+    })
+    nicknameList = await nicknameList.json();
+    nickname = nicknameList[0];
+    document.getElementById('profile-nickname').value = nickname;
+
+});
+
+
+
+
+
+
