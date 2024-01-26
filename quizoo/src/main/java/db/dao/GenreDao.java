ebcd41e5bdf3d925254cc36c5df8be020bcb66bd
st22304013 <st22304013@ttc.ac.jp>
2024-01-26 14:27:59 +0900

    ジャンル一覧を取得するメソッド作成

--- quizoo/src/main/java/db/dao/GenreDao.java
+++ quizoo/src/main/java/db/dao/GenreDao.java
@@ -1,11 +1,43 @@
 package db.dao;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import db.bean.QuizBean;
 import frame.exception.ResourceException;
 
 public class GenreDao extends Dao {
-	
+	public ArrayList<QuizBean> selectGenre() throws ResourceException {
+		ArrayList<QuizBean> genreList = new ArrayList<>();
+		
+		connect();
+		
+		String sql = "SELECT * FROM quiz ORDER BY genre_no ASC";
+		
+		
+		try {
+			st = cn.prepareStatement(sql);
+			
+			rs = st.executeQuery();
+			
+			
+			while (rs.next()) {
+                QuizBean quiz = new QuizBean();
+                quiz.setGenreNo(rs.getInt("genre_no"));
+                quiz.setGenre(rs.getString("genre"));
+
+                genreList.add(quiz);
+            }
+			
+			
+			
+		} catch (SQLException e) {
+			throw new ResourceException(e.getMessage(), e);
+		}
+		
+		close();
+		
+		return genreList;
+		
+	}
 }
