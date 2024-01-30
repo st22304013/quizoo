3ccc75f32838c73da4c889349320c41021435103
st22304013 <st22304013@ttc.ac.jp>
2024-01-30 11:40:25 +0900

    ハンバーガメニューのscriptをindex.jsに移動完了

--- quizoo/src/main/webapp/js/index.js
+++ quizoo/src/main/webapp/js/index.js
@@ -24,6 +24,37 @@ window.addEventListener('load',function(){
         list_box.replaceWith(list); 
     
     })();
+
+    // ハンバーガーボタンクリックで実行
+    document.querySelector(".drawer__button").addEventListener('click', function () {
+        this.classList.toggle("active");
+        document.querySelector(".drawer__nav").classList.toggle("active");
+    });
+
+    // ドロワーナビゲーションリンククリックで非アクティブ化
+    document.querySelectorAll(".drawer__nav__link").forEach(function (link) {
+        link.addEventListener('click', function () {
+            document.querySelector(".drawer__button").classList.remove("active");
+            document.querySelector(".drawer__nav").classList.remove("active");
+        });
+    });
+
+    // ページ内スクロール
+    document.querySelectorAll('a[href^="#"]').forEach(function (anchor) {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            const speed = 400;
+            let href = this.getAttribute("href");
+            let target = document.querySelector(href === "#" || href === "" ? "html" : href);
+            let position = target.offsetTop;
+
+            window.scrollTo({
+                top: position,
+                behavior: "smooth"
+            });
+        });
+    });
 })
 
 async function getQuizList() {
