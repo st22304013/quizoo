c6cef8203f4f739bc7c5e50b6bf88200df0882c7
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-30 16:36:13 +0900

    わからないです。へｌぺめ

--- quizoo/src/main/webapp/js/profile.js
+++ quizoo/src/main/webapp/js/profile.js
@@ -12,8 +12,35 @@ window.addEventListener("load", async () =>{
 
 });
 
+function sendData() {
+    const XHR = new XMLHttpRequest();
 
+    // FormData オブジェクトと form 要素を紐づけます
+    const FD = new FormData(form);
 
+    // データが正常に送信された場合に行うことを定義します
+    XHR.addEventListener("load", (event) => {
+      alert(document.getElementById('profile-nickname').value);
+    });
 
+    // エラーが発生した場合に行うことを定義します
+    XHR.addEventListener("error", (event) => {
+      alert("Oops! Something went wrong.");
+    });
 
+    // リクエストをセットアップします
+    XHR.open("POST", "updatenickname");
 
+    // 送信したデータは、ユーザーがフォームで提供したものです
+    XHR.send(document.getElementById('profile-nickname').value);
+  }
+
+  // form 要素を取得
+  const form = document.getElementById("login-form");
+
+  // 'submit' イベントのハンドラーを追加
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    sendData();
+  });
