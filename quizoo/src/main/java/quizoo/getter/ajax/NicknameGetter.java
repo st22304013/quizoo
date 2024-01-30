960afb9968f5caf3b40c108a54bd7755deb2fe4f
st22304013 <st22304013@ttc.ac.jp>
2024-01-30 10:02:31 +0900

    execute()を空で実装しただけ

--- quizoo/src/main/java/quizoo/getter/ajax/NicknameGetter.java
+++ quizoo/src/main/java/quizoo/getter/ajax/NicknameGetter.java
@@ -1,5 +1,19 @@
 package quizoo.getter.ajax;
 
-public class NicknameGetter {
+import java.io.IOException;
 
+import frame.Service;
+import frame.context.RequestContext;
+import frame.context.ResponseContext;
+import frame.exception.BadRequestException;
+import frame.exception.NotFoundException;
+import frame.exception.ResourceException;
+
+public class NicknameGetter extends Service {
+	
+	@Override
+	public void execute(RequestContext req, ResponseContext res)
+			throws IOException, ResourceException, BadRequestException, NotFoundException {
+		
+	}
 }
