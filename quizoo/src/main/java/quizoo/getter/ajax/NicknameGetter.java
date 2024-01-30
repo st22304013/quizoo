ca83a5c8a3f7c6b5f26c5a79c7d51515de5d0c02
st22304013 <st22304013@ttc.ac.jp>
2024-01-30 10:30:33 +0900

    NicknameGetter完成(仮)

--- quizoo/src/main/java/quizoo/getter/ajax/NicknameGetter.java
+++ quizoo/src/main/java/quizoo/getter/ajax/NicknameGetter.java
@@ -1,7 +1,11 @@
 package quizoo.getter.ajax;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
+import com.google.gson.Gson;
+
+import db.dao.UserInfoDao;
 import frame.Service;
 import frame.context.RequestContext;
 import frame.context.ResponseContext;
@@ -14,6 +18,18 @@ public class NicknameGetter extends Service {
 	@Override
 	public void execute(RequestContext req, ResponseContext res)
 			throws IOException, ResourceException, BadRequestException, NotFoundException {
-		
+		// TODO 自動生成されたメソッド・スタブ
+				
+				int userNo = req.getUser().getUserNo();
+				UserInfoDao userInfoDao = new UserInfoDao();
+				
+				String nickname = userInfoDao.selectNickname(userNo);
+				
+				PrintWriter out = res.getWrite();
+				
+				Gson gson = new Gson();
+				String result = gson.toJson(nickname);
+				
+				out.println(result);
 	}
 }
