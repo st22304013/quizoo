25b483767cd3f9d8483184d2cadc49fcb1fcc52b
wataru-51 <kw22304025@ga.ttc.ac.jp>
2024-01-31 14:55:25 +0900

    ようわからんけど、fetchで送り先指定して メッセージボディを直接書いてる。多分

--- quizoo/src/main/webapp/js/profile.js
+++ quizoo/src/main/webapp/js/profile.js
@@ -2,8 +2,8 @@
  * 
  */
 let nicknameList;
-window.addEventListener("load", async () =>{
-    nicknameList = await fetch("/quizoo/nickname",{
+window.addEventListener("load", async () => {
+    nicknameList = await fetch("/quizoo/nickname", {
         credentials: "include"
     })
     nicknameList = await nicknameList.json();
@@ -13,34 +13,25 @@ window.addEventListener("load", async () =>{
 });
 
 function sendData() {
-    const XHR = new XMLHttpRequest();
+}
 
-    // FormData オブジェクトと form 要素を紐づけます
-    const FD = new FormData(form);
 
-    // データが正常に送信された場合に行うことを定義します
-    XHR.addEventListener("load", (event) => {
-      alert(document.getElementById('profile-nickname').value);
-    });
+// 'submit' イベントのハンドラーを追加
+form.addEventListener("submit", async (event) => {
+    var newName = document.querySelector("#profile-nickname");
 
-    // エラーが発生した場合に行うことを定義します
-    XHR.addEventListener("error", (event) => {
-      alert("Oops! Something went wrong.");
-    });
+    console.log(newName.value);
 
-    // リクエストをセットアップします
-    XHR.open("POST", "updatenickname");
+    var result = fetch("/quizoo/updatenickname?nickname=test",{
+        head:{
+            "Content-Type":"application/json"
+        },
+        body:`{nickname=${newName.value}}`,
+        method:"post",
+        credentials:"include",
+    })
 
-    // 送信したデータは、ユーザーがフォームで提供したものです
-    XHR.send(document.getElementById('profile-nickname').value);
-  }
-
-  // form 要素を取得
-  const form = document.getElementById("login-form");
-
-  // 'submit' イベントのハンドラーを追加
-  form.addEventListener("submit", (event) => {
     event.preventDefault();
 
     sendData();
-  });
+});
