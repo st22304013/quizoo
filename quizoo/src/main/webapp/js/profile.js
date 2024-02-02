8dda32620523f4a2c6ecfbca1c568482cad6f24b
komamura <ek22304009@ga.ttc.ac.jp>
2024-02-02 15:57:50 +0900

    ニックネームのコメントをなくしただけ

--- quizoo/src/main/webapp/js/profile.js
+++ quizoo/src/main/webapp/js/profile.js
@@ -3,40 +3,40 @@
  */
 const form = document.getElementById("login-form");
 
-//let nicknameList;
-//window.addEventListener("load", async () => {
-//    nicknameList = await fetch("/quizoo/nickname", {
-//        credentials: "include"
-//    })
-//    nicknameList = await nicknameList.json();
-//    nickname = nicknameList[0];
-//    document.getElementById('profile-nickname').value = nickname;
-//
-//});
+let nicknameList;
+window.addEventListener("load", async () => {
+    nicknameList = await fetch("/quizoo/nickname", {
+        credentials: "include"
+    })
+    nicknameList = await nicknameList.json();
+    nickname = nicknameList[0];
+    document.getElementById('profile-nickname').value = nickname;
+
+});
 
 function sendData() {
 }
 
 
-//// 'submit' イベントのハンドラーを追加
-//form.addEventListener("submit", async (event) => {
-//    var newName = document.querySelector("#profile-nickname");
-//
-//    console.log(newName.value);
-//
-//    var result = fetch("/quizoo/updatenickname?nickname=test",{
-//        head:{
-//            "Content-Type":"application/json"
-//        },
-//        body:`{nickname=${newName.value}}`,
-//        method:"post",
-//        credentials:"include",
-//    })
-//
-//    event.preventDefault();
-//
-//    sendData();
-//});
+//// 'submit' イトのハンドラーを追加
+form.addEventListener("submit", async () => {
+    var newName = document.querySelector("#profileickname");
+
+    console.log(nme.value);
+
+    var result = fetch("/quizoo/updatenickname?nime=test",{
+      head:{
+            "Content-Type":"applition/json"
+        },
+        body:`{nickname=${newName.value}}`,
+      meod:"post",
+        credentials:"include",
+    })
+
+    event.preventDefault();
+
+    sendData();
+});
 
 // 'submit' イベントのハンドラーを追加
 form.addEventListener("submit", async (event) => {
