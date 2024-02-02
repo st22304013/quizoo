dc735cb5eff53d9bfa6e76d263bfe1c13f68523d
st22304013 <st22304013@ttc.ac.jp>
2024-02-02 16:02:41 +0900

    CreateHistoryBean完成

--- quizoo/src/main/java/db/bean/CreateHistoryBean.java
+++ quizoo/src/main/java/db/bean/CreateHistoryBean.java
@@ -1,5 +1,72 @@
 package db.bean;
 
-public class CreateHistoryBean {
+import java.io.Serializable;
 
+public class CreateHistoryBean implements Serializable {
+	private String title;
+	private String explanation;
+	
+	private String nickname;
+	private String createTime;
+	private String genre;
+	private float correctRate;
+	
+	public CreateHistoryBean() {}
+	
+	public CreateHistoryBean(String title, String explanation, String nickname, String createTime, String genre, float correctRate) {
+		this.setTitle(title);
+		this.setExplanation(explanation);
+		this.setNickname(nickname);
+		this.setCreateTime(createTime);
+		this.setGenre(genre);
+		this.setCorrectRate(correctRate);
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getExplanation() {
+		return explanation;
+	}
+
+	public void setExplanation(String explanation) {
+		this.explanation = explanation;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public float getCorrectRate() {
+		return correctRate;
+	}
+
+	public void setCorrectRate(float correctRate) {
+		this.correctRate = correctRate;
+	}
 }
