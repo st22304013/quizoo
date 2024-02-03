f88223e79b860327a580e691e3b57daed06b9b24
sora-jbk <aj22304027@ga.ttc.ac.jp>
2024-02-03 16:34:57 +0900

    コメント追加

--- quizoo/src/main/webapp/js/createQuiz.js
+++ quizoo/src/main/webapp/js/createQuiz.js
@@ -14,13 +14,17 @@ function showQreateModal() {
 
 function storeQuestionEditor() {
     currentEditor = document.querySelector("#question-editor");
+
+    // 作成中の情報を保存
     questionEditors[EditingQuestionNo] = currentEditor;
     
+
+    // 空のクエスチョン一覧を作成
     var storedQuestion = document.createElement("div");
     storedQuestion.setAttribute("class","stored-question");
     storedQuestion.setAttribute("id","stored-question");
 
-
+    // questionEditorsから情報を取り出し、クエスチョン一覧に表示
     for(let i = 0 ; i < questionEditors.length ; i++) {
         var overview = document.createElement("div");
         overview.setAttribute("class","question-overview");
@@ -28,6 +32,7 @@ function storeQuestionEditor() {
         storedQuestion.appendChild(overview);
     }
 
+    // 追加ボタンをクローン
     storedQuestion.appendChild(document.querySelector("#add-question").cloneNode(true));
     
     document.querySelector("#stored-question").replaceWith(storedQuestion.cloneNode(true));
