23308e25a9330beb90975ae4cf7bdb8b4b89e437
sora-jbk <aj22304027@ga.ttc.ac.jp>
2024-02-03 17:19:41 +0900

    クイズ追加時にエディタを初期化

--- quizoo/src/main/webapp/js/createQuiz.js
+++ quizoo/src/main/webapp/js/createQuiz.js
@@ -5,7 +5,7 @@ window.addEventListener("load",function () {
     createModal = this.window.document.querySelector("#create-quiz-modal-back"); 
 
     this.document.querySelector("#create-quiz-open").addEventListener("click",showQreateModal);
-    this.document.querySelector("#add-question").addEventListener("click",storeQuestionEditor);
+    this.document.querySelector("#add-question").addEventListener("click",addQuestion);
 })
 
 function showQreateModal() {
@@ -30,16 +30,78 @@ function storeQuestionEditor() {
         var overview = document.createElement("div");
         overview.setAttribute("class","question-overview");
         overview.innerText = questionEditors[i].querySelector("#question-text").value;
+        overview.addEventListener("click",changeQuestionEditor.bind(null,i));
         storedQuestion.appendChild(overview);
     }
 
     // 追加ボタンをクローン
     var addBtn = document.querySelector("#add-question").cloneNode(true);
     // イベントはcloneNodeで継承されないので、ここで再設定
-    addBtn.addEventListener("click",storeQuestionEditor);
+    addBtn.addEventListener("click",addQuestion);
     storedQuestion.appendChild(addBtn);
     
     // クエスチョン一覧を置き換え
     document.querySelector("#stored-question").replaceWith(storedQuestion);
 
+}
+
+//　編集する問題を切り返す
+function changeQuestionEditor(questionNo){
+    // 現在開いている問題と同じときは何もしない
+    if(EditingQuestionNo == questionNo) return;
+    // questionNoが無いときは何もしない
+    if(!questionNo) return;
+
+    // 現在開いている問題を保存
+    storeQuestionEditor();
+
+    document.querySelector("#question-editor").replaceWith(questionEditors[questionNo].cloneNode(true));
+} 
+
+// クエスチョンを追加する
+function addQuestion(){
+    // 現在作成中のクエスチョンを保存
+    storeQuestionEditor();
+    
+    let emptyEditor;
+    // 初回のみ空のクエスチョンを作成
+    if(!emptyEditor){
+        emptyEditor = document.createElement("div");
+        emptyEditor.setAttribute("class","question-editor");
+        emptyEditor.setAttribute("id","question-editor");
+
+        var text = document.createElement("textarea");
+        text.setAttribute("type","text");
+        text.setAttribute("name","question-text");
+        text.setAttribute("class","question-text");
+        text.setAttribute("id","question-text");
+        emptyEditor.appendChild(text);
+
+        var filedset = document.createElement("fieldset");
+        filedset.setAttribute("class","choices");
+        for(let i = 1 ; i <= 4 ; i++){
+            var choice = document.createElement("div");
+            choice.setAttribute("class","choice");
+
+            var radio = document.createElement("input");
+            radio.setAttribute("type","radio");
+            radio.setAttribute("name","choice");
+            radio.setAttribute("id",i);
+            choice.appendChild(radio);
+
+            var text = document.createElement("input");
+            text.setAttribute("type","text");
+            text.setAttribute("name","choisce-text");
+            text.setAttribute("class","choice-text");
+            text.setAttribute("id","choice-text");
+
+            choice.appendChild(text);
+
+            filedset.appendChild(choice);
+        }
+        emptyEditor.appendChild(filedset);
+    }
+
+    EditingQuestionNo = questionEditors.length;
+    document.querySelector("#question-editor").replaceWith(emptyEditor.cloneNode(true));
 }
