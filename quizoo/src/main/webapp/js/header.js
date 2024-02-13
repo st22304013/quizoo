0f7cc1098aeda5b616c57fa9330d9e1a8a75ce8d
sora-jbk <aj22304027@ga.ttc.ac.jp>
2024-02-13 09:45:58 +0900

    デザインを修正

--- quizoo/src/main/webapp/js/header.js
+++ quizoo/src/main/webapp/js/header.js
@@ -5,13 +5,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     // ハンバーガーボタンクリックで実行
     document.querySelector("#side-menu-drawer").addEventListener('click', function () {
-        document.querySelector("#side-menu").style.display = "block";
+        document.querySelector("#side-menu").classList.add("show");
     });
 
     // ドロワーナビゲーションリンククリックで非アクティブ化
     document.querySelector("#side-menu").addEventListener('click',function () {
         if(event.target == document.querySelector("#side-menu-body")) return;
-        document.querySelector("#side-menu").style.display = "none";
+        document.querySelector("#side-menu").classList.remove("show");
     });
 
     // ページ内スクロール
